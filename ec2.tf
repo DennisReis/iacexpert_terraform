ec6668ff3118045f0fb5e1027ba5828bd52c9502
Dennis Marinho <dmpreis@gmail.com>
2020-07-11 16:32:43 -0300

    Inital commit

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["IaaSWeek-${var.hash_commit}"] 
+    }
+
+    owners = ["178520105998"]
+}
+
+resource "aws_instance" "web" {
+    count         = 2
+    ami           = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+
+    tags = {
+        Name     = "HelloWorld"
+        Training = "linuxtips"
+        Subject  = "terraform"
+    }
+}
