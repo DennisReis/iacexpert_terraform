322db3b82f00fe97bf5b89f0cbce1bcb244c0115
Dennis Marinho <dmpreis@gmail.com>
2020-11-16 16:42:58 -0300

    remove backend S3 to work with Terraform Cloud

--- main.tf
+++ main.tf
@@ -3,12 +3,13 @@ provider "aws" {
   version = "~>2.0"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "dmprlab-tfstates-terraform"
-    key    = "terraform-test.tfstate"
-    #dynamodb_table = "terraform-state-lock-dynamo"
-    region = "us-east-1"
-    #encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "dmprlab-tfstates-terraform"
+#    key    = "terraform-test.tfstate"
+#    #dynamodb_table = "terraform-state-lock-dynamo"
+#    region = "us-east-1"
+#    #encrypt        = true
+#  }
+#}
+#
